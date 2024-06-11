e7c51e8f4b77ac814167318fc4dc3b68df1dd4c2
karen508 <131675378+karen508@users.noreply.github.com>
2024-06-11 09:40:23 -0300

    sdg

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes= documents.querySelectorAll(".botao")
+
+for(let i=0; i <botoes.length;j++){
+    botoes[i]. onclick = function()
+    {for(let j=o;j<botoes.length;j++){
+        botoes[j].classList.remove("ativo");
+    }
+botoes[i].classList.add("ativo");
+}
+}
